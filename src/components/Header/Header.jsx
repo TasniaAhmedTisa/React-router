9ab8e97da0bff4a1895a7b6863d9a82ca01cb7cc
TasniaAhmedTisa <tasniaahmed055@gmail.com>
2024-10-30 00:03:57 +0600

    1st commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <nav className="m-4">
+                
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
